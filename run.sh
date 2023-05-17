bb8181d6ddb090f052f471b5a105ad9d9bd9137b
Arkadiusz Wolk <awolk@student.agh.edu.pl>
2023-05-17 22:56:50 +0200

    setup cilium & hubble

--- run.sh
+++ run.sh
@@ -1,13 +1,23 @@
-#!bin/bash
+#!/bin/bash
 
 set -x
 
+# start minikube
+minikube start --network-plugin=cni --cni=false
+
+# install cilium
+cilium install
+
+# enable Hubble
+cilium hubble enable
+cilium hubble port-forward&
+
+# use minikube's docker to be able to load local images
 eval $(minikube docker-env)
 
+# build and load service image
 docker build -t simple-service:latest ./simple-service/
-
 minikube image load simple-service
 
+# deploy service
 kubectl create deployment simple-service --image=simple-service
-
-minikube image rm simple-service
